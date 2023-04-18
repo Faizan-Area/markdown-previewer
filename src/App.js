7a595102ba1c0013ac8f344f2f7267288bec0a04
Faizan Ahmed <faizanbca2021@gmail.com>
2023-04-18 20:01:59 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { marked } from 'marked';
 
 function App() {
+  const [text, setText] = useState('');
+  const valueChange = (event) => {
+    setText(event.target.value)
+  }
+  marked.setOptions({
+    breaks: true
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <span className="navbar-brand" href="#">Navbar</span>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <span className="nav-link active" aria-current="page" href="#">Home</span>
+              <span className="nav-link" href="#">Features</span>
+              <span className="nav-link" href="#">Pricing</span>
+              <span className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</span>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <main className="App">
+        <div className="container">
+          <h2>Editor</h2>
+          <textarea name="editor" id="editor" cols="30" rows="10" onChange={valueChange}></textarea>
+        </div>
+        <div className="container">
+          <h2>Preview</h2>
+          <div id="preview" dangerouslySetInnerHTML={{__html: marked(text),}}></div>
+        </div>
+      </main>
     </div>
   );
 }
